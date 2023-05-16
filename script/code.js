b1254d9dbc0336b8ffaeeba89ff9085faff3a3e9
Thabangx10 <Thabangx12@gmail.com>
2023-05-16 16:06:44 +0200

    main

--- script/code.js
+++ script/code.js
@@ -11,10 +11,18 @@ let Subjects = ['Python', 'C++', 'VB.Net', 'Javascript', 'Assembly'];
  
 // We are going to create a variable to contain our last index value
 
-let last = Subjects.slice(-1)[0];
+let last = Subjects.slice(4)[0];
+// let last = Subjects[Subjects.length - 1]
+// let last = Subjects.slice(-1);
+// document.getElementById('lastone').innerHTML += last;
 console.log(last);
 
 // To find the middle element, we will also create a variable container
 
-let middle = Subjects.slice(-3)[0];
+// let middle = Subjects.slice(2)[0];
+// let middle1 = Subjects[Subjects.length  - 3]
+// console.log(middle1);
+ 
+let middle = Subjects.slice(2)[0];
+document.getElementById('middleone').innerHTML += middle;
 console.log(middle);
